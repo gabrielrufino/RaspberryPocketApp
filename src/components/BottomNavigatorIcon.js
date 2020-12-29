d4c92a8d3374bc5b7476a760249a5fe7622ec5d4
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-29 14:16:20 -0300

    New component: BottomNavigatorIcon

---
+++ src/components/BottomNavigatorIcon.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+const BottomNavigatorIcon = ({name, color, size}) => {
+  return <Icon name={name} color={color} size={size} />;
+};
+
+export default BottomNavigatorIcon;
