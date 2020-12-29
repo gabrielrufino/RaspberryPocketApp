30f81912de273a7ae01bd625fe25181ae78162ef
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-29 14:47:43 -0300

    New routes in bottom navigator: Status, Commands and Settings

--- src/BottomNavigator.js
+++ src/BottomNavigator.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+import BottomNavigatorIcon from './components/BottomNavigatorIcon';
 import {Commands, Status} from './screens';
 
 const Tab = createBottomTabNavigator();
 
 const BottomNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Status} />
-      <Tab.Screen name="Commands" component={Commands} />
+    <Tab.Navigator tabBarOptions={{activeTintColor: '#e91e63'}}>
+      <Tab.Screen
+        name="Status"
+        component={Status}
+        options={{
+          tabBarLabel: 'Raspberry Status',
+          tabBarIcon: ({color, size}) => (
+            <BottomNavigatorIcon
+              name="raspberry-pi"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Commands"
+        component={Commands}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <BottomNavigatorIcon name="terminal" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={() => <></>}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <BottomNavigatorIcon name="cog" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
