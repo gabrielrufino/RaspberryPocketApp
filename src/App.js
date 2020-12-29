68cf05fd61ef4a68a86251c10e80c36d10c98b17
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-29 13:40:17 -0300

    Connecting to socket

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Text, StatusBar} from 'react-native';
 
 import BottomNavigator from './BottomNavigator';
 
 const App = () => {
+  useEffect(() => {
+    import('./services/server').then((server) => {
+      server.socket.on('connect', () => {
+        console.log('Connected to the server');
+      });
+    });
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
