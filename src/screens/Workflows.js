b8e676f832989ebe49e82aca3da18eac28d406db
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-31 10:51:48 -0300

    New workflow: restart

--- src/screens/Workflows.js
+++ src/screens/Workflows.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useCallback, useMemo} from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import {StyleSheet, View} from 'react-native';
+import {Avatar, ListItem} from 'react-native-elements';
+
+import server from '../services/server';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFF',
+    flex: 1,
+  },
+});
 
 const Status = () => {
+  const workflows = useMemo(
+    () => [
+      {
+        icon: 'undo',
+        title: 'Restart',
+        subtitle: 'Reboot your raspberry pi',
+        action: 'request:workflow:restart',
+      },
+    ],
+    [],
+  );
+
+  const requestWorkflow = useCallback((workflow) => {
+    server.socket.emit(workflow.action);
+  }, []);
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      {workflows.map((workflow) => (
+        <ListItem bottomDivider onPress={() => requestWorkflow(workflow)}>
+          <Avatar containerStyle={{alignItems: 'center'}}>
+            <Icon name={workflow.icon} color="#AAAAAA" size={30} />
+          </Avatar>
+          <ListItem.Content>
+            <ListItem.Title>{workflow.title}</ListItem.Title>
+            <ListItem.Subtitle>{workflow.subtitle}</ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+      ))}
     </View>
   );
 };
